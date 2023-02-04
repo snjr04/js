1128b7c92cf73244b71a92f32e04e3161706e923
snjr04 <superkingbro004@gmail.com>
2023-02-04 13:58:12 +0300

    '1-hm'

---
+++ main.js
@@ -0,0 +1,55 @@
+let a =5
+
+let test = 'hello Sanjar'
+
+console.log(a)
+
+let b = 'Sanjar'
+
+console.log(test)
+
+a=12
+
+console.log(a)
+
+let n = 18
+
+// alert(n)
+
+let  str ='hello world '
+
+str= 50
+
+console.log(str)
+
+
+let srt = '123'
+
+console.log(typeof srt)
+
+let tru = false
+
+if ( tru == false){
+    tru = 0
+}
+
+console.log(tru )
+
+let u = undefined
+
+ if (u==undefined){
+        u=5
+}
+
+console.log (u)
+
+
+// let persone = {
+//     name: 'Sanjar',
+//     age: 18,
+//     car: {
+//         color: 'black'
+//
+//
+//     }
+// }
